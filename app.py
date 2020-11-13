173fc2bf9f969f40dccbc34e0c20c1aa378f45bc
MM12583 <lordarcher12345@gmail.com>
2020-11-13 18:03:23 +0800

    bassic

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__) 
+
+@app.route('/')
+def home() : 
+    return 'Hello Flask this is the main page <h1>Hello!<h1>'
+
+if __name__ == '__main__' :
+    app.run()
